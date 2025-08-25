a438529c26098cfbb343c4048f38696a47659cf0
kelvyn2012 <kudufo@gmail.com>
2025-08-25 19:09:13 +0100

    added my user app and created the models for user

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "rest_framework",
     "django_filters",
+    "rest_framework.authtoken",
     "core",
-    "user",
-    "catalog" "django.contrib.messages",
+    "catalog",
+    "users",
+    "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
 
@@ -134,18 +136,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
-    "DEFAULT_FILTER_BACKENDS": (
-        "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.SearchFilter",
-        "rest_framework.filters.OrderingFilter",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
+
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
