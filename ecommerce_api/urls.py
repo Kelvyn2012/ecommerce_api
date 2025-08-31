ad1cfa0f284bb193ae39ec8bd006ad59007e5bb8
kelvyn2012 <kudufo@gmail.com>
2025-09-01 00:09:45 +0100

    added a home route

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -4,13 +4,20 @@ from rest_framework.routers import DefaultRouter
 from catalog.views import ProductViewSet, CategoryViewSet
 from users.views import UserViewSet, RegisterView, login_view
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import JsonResponse
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
 router.register(r"categories", CategoryViewSet, basename="category")
 router.register(r"products", ProductViewSet, basename="product")
 
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the E-commerce API 🚀"})
+
+
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/auth/token/", obtain_auth_token, name="api_token_auth"),
